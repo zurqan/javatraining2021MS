eabc5bc9a9279a9af0866e6de71efd2ec06b7cc8
malzurqan <zurqan@gmail.com>
2021-06-27 13:49:22 +0300

    session 2 - spring boot introduction

---
+++ 1_introduction/introduction-step-2/src/main/java/com/training/introductionstep2/IntroductionStep2Application.java
@@ -0,0 +1,13 @@
+package com.training.introductionstep2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionStep2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionStep2Application.class, args);
+	}
+
+}
