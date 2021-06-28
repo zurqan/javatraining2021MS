35b7e73f9e134cbf286314579881768ffc6624e6
malzurqan <zurqan@gmail.com>
2021-06-28 13:17:41 +0300

    Service Registry

--- 2_service_registry/service-a/src/main/java/com/training/servicea/ServiceAApplication.java
+++ 2_service_registry/service-a/src/main/java/com/training/servicea/ServiceAApplication.java
@@ -1,11 +1,16 @@
 package com.training.servicea;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -20,5 +25,16 @@ public class ServiceAApplication {
     @LoadBalanced
     public RestTemplate restTemplate(){
 	    return new RestTemplate();
+    }
+
+    @Bean
+    CommandLineRunner test(DiscoveryClient discoveryClient){
+	    return args->{
+            List<ServiceInstance> instances = discoveryClient.getInstances("service-b");
+
+            instances
+                    .stream()
+                    .forEach(System.out::println);
+        };
     }
 }
