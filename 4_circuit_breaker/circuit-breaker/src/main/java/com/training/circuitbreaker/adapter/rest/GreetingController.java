be7fe989e4fa994b9ad85775574d9cf36c1eb5cc
malzurqan <zurqan@gmail.com>
2021-07-01 10:56:47 +0300

    cb-yml config

--- 4_circuit_breaker/circuit-breaker/src/main/java/com/training/circuitbreaker/adapter/rest/GreetingController.java
+++ 4_circuit_breaker/circuit-breaker/src/main/java/com/training/circuitbreaker/adapter/rest/GreetingController.java
@@ -1,5 +1,6 @@
 package com.training.circuitbreaker.adapter.rest;
 
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    private final CircuitBreakerFactory circuitBreakerFactory;
+
+    public GreetingController(CircuitBreakerFactory circuitBreakerFactory) {
+        this.circuitBreakerFactory = circuitBreakerFactory;
+    }
+
     @GetMapping("/{name}")
-    public String greeting(@PathVariable String name){
-        return sayHello(name);//cb .. name start with A 10 sec.. error -> default message "Hello!"
+    public String greeting(@PathVariable String name) {
+//        return sayHello(name);//cb .. name start with A 10 sec.. error -> default message "Hello!"
+
+        return circuitBreakerFactory
+                .create("hello")
+                .run(() -> sayHello(name),
+                        th -> sayDefaultHello());
+    }
+
+    private String sayDefaultHello() {
+        return "Hello!";
     }
 
     private String sayHello(String name) {
+
+        if(name.startsWith("A")){
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         return "Hello " + name;
     }
 
