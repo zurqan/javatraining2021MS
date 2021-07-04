019ad6b08c309079976ed7f191d5ae2f7bf6cc2d
malzurqan <zurqan@gmail.com>
2021-07-04 12:08:16 +0300

    command handler

---
+++ 6-spring-ext/command-handler/src/main/java/com/training/commandhandler/CommandHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.training.commandhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandHandlerApplication.class, args);
+	}
+
+}
