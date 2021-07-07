23c377a40c5d1de1073201b869615d9725b046f5
malzurqan <zurqan@gmail.com>
2021-07-07 12:50:50 +0300

    b

---
+++ 9-messaging/rabbitmq-spring-introduction/src/main/java/com/training/rabbitmqspringintroduction/RabbitmqSpringIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.training.rabbitmqspringintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqSpringIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqSpringIntroductionApplication.class, args);
+	}
+
+}
---
+++ 9-messaging/rabbitmq-spring-introduction/src/test/java/com/training/rabbitmqspringintroduction/RabbitmqSpringIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.rabbitmqspringintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqSpringIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
