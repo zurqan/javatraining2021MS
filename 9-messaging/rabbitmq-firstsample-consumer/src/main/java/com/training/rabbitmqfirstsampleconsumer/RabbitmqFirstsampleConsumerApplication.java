106f7b8e85f719533d1b9af56a7b24c4731445ba
malzurqan <zurqan@gmail.com>
2021-07-07 10:54:43 +0300

    messaging

---
+++ 9-messaging/rabbitmq-firstsample-consumer/src/main/java/com/training/rabbitmqfirstsampleconsumer/RabbitmqFirstsampleConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.training.rabbitmqfirstsampleconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqFirstsampleConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqFirstsampleConsumerApplication.class, args);
+	}
+
+}
